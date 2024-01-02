c272639aa2437fe1350c34d625aecc3e14580c9c
jakegodsall <jake.edward.godsall@gmail.com>
2024-01-02 12:30:42 +0100

    modified logic to allow for deselecting active item

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,16 @@
-// query selector
 const questions = document.querySelectorAll(".question-answer");
 
 questions.forEach(function (question) {
-  const btn = question.firstElementChild.lastElementChild;
-
-  btn.addEventListener("click", function () {
-    questions.forEach(function (question) {
-      if (question.classList.contains("active")) {
-        question.classList.remove("active");
-      }
-    });
-    const answer = btn.parentNode.nextElementSibling;
-    question.classList.add("active");
-
-    // Set height of element
+  question.addEventListener("click", function () {
+    if (question.classList.contains("active")) {
+      question.classList.remove("active");
+    } else {
+      questions.forEach(function (otherQuestion) {
+        if (otherQuestion.classList.contains("active")) {
+          otherQuestion.classList.remove("active");
+        }
+      });
+      question.classList.add("active");
+    }
   });
 });
