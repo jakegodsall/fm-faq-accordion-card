d7e59f01808d0c605306530241b3ee1afd62b79a
jakegodsall <jake.edward.godsall@gmail.com>
2024-01-02 12:24:35 +0100

    added smooth transitions for FAQ items

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,18 @@
 // query selector
-const questions = document.querySelectorAll('.question-answer');
+const questions = document.querySelectorAll(".question-answer");
 
 questions.forEach(function (question) {
+  const btn = question.firstElementChild.lastElementChild;
 
-    const btn = question.firstElementChild.lastElementChild;
+  btn.addEventListener("click", function () {
+    questions.forEach(function (question) {
+      if (question.classList.contains("active")) {
+        question.classList.remove("active");
+      }
+    });
+    const answer = btn.parentNode.nextElementSibling;
+    question.classList.add("active");
 
-    btn.addEventListener('click', function () {
-        questions.forEach(function (question) {
-            if (question.classList.contains('active')) {
-                question.classList.remove('active');
-            }
-        })
-        const answer = btn.parentNode.nextElementSibling;
-        question.classList.add('active');
-
-    })
+    // Set height of element
+  });
 });
